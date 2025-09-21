910161521143212133999ac3aa19fd5276541193
Alazo4872 <lazoal4872@gmail.com>
2025-09-21 02:46:18 -0500

    fix: fallback directory

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get('/api/notes/:id', (request, response) => {
     response.status(404).end()
   }
 })
-app.get('/*', (req, res) => {
+app.get('/.*/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 app.use(unknownEndpoint)
